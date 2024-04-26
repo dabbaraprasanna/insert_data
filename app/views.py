c1aa52e6e82b4de0bd310ff50c629268b4610aaa
dabbaraprasanna <prasannalakshmidabbara23@gmail.com>
2024-04-26 21:00:05 +0530

    firstcommit

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ def insert_topic(request):
     TO.save()
     return HttpResponse('Topic is created successfully')
 
+'''
 def insert_webpage(request):
     tn=input('enter name')
     TO=Topic.objects.get_or_create(topic_name=tn)[0]
@@ -18,7 +19,26 @@ def insert_webpage(request):
     u=input('enter url')
     WO=webpage.objects.get_or_create(name=n,url=u,topic_name=TO)[0]
     WO.save()
-    return HttpResponse('webpage is created successfully')
+    return HttpResponse('webpage is created successfully')'''
+
+
+
+def insert_webpage(request):
+    tn=input('enter tn')
+    n=input('enter name')
+    u=input('enter url')
+
+
+    #TO=Topic.objects.get_or_create(topic_name=tn)
+    ''' we can use get method to get the parent table object but if parent table object is not avaia;able it throws an error'''
+    LTO=Topic.objects.filter(topic_name=tn)
+    if LTO:
+       RTO=LTO[0]
+       WO=webpage.objects.get_or_create(name=n,url=u,topic_name=RTO)[0]
+       WO.save()
+       return HttpResponse('webpage is created successfully')
+    else:
+        return HttpResponse('given topic is Not  present in my parent table')
 
 def insert_access(request):
     tn=input('enter topic')
